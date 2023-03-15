f8ac5b2439b1e5e06ff3e3c6cad889a2a0e74e62
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-03-15 12:17:34 +0330

    Add main function to get the args

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import platform
 import subprocess
+import sys
 
 
 def set_dns(primary_ip, secondary_ip):
@@ -22,3 +23,21 @@ def unset_dns():
         subprocess.run(["networksetup", "-setdnsservers", "Wi-Fi", "Empty"])
     elif system == "Linux":
         subprocess.run(["nmcli", "connection", "modify", "Wi-Fi", "ipv4.ignore-auto-dns", "yes"])
+
+
+def main():
+    args = sys.argv
+    if len(args) == 1:
+        unset_dns()
+    elif args == 3:
+        primary_dns = args[1]
+        secondary_dns = args[2]
+        set_dns(primary_dns, secondary_dns)
+    else:
+        print("Error: Not Enough Arguments")
+        print("Usage -> `python3 core.py` to unset dns")
+        print("Usage -> `python3 core.py <primary_dns> <secondary_dns>` to unset dns")
+
+
+if __name__ == "__main__":
+    main()

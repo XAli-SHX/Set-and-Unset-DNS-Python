8ecea8a90bed4fb5b2cdd8b9074169b19ece56bf
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-03-15 13:45:12 +0330

    Fix windows administrator problem

--- core.py
+++ core.py
@@ -1,14 +1,21 @@
 import platform
 import subprocess
 import sys
+import pyuac
 
 
 def set_dns(primary_ip, secondary_ip):
     system = platform.system()
     if system == "Windows":
-        subprocess.run(["netsh", "interface", "ip", "set", "dns", "name=\"Wi-Fi\"", "static", primary_ip, "primary"])
+        if not pyuac.isUserAdmin():
+            print("Re-launching as admin!")
+            pyuac.runAsAdmin()
+            return
         subprocess.run(
-            ["netsh", "interface", "ip", "add", "dns", "name=\"Wi-Fi\"", "addr={}".format(secondary_ip), "index=2"])
+            ["netsh", "interface", "ipv4", "add", "dnsserver", "\"Wi-Fi\"", "address={}".format(primary_ip), "index=1"])
+        subprocess.run(
+            ["netsh", "interface", "ipv4", "add", "dnsserver", "\"Wi-Fi\"", "address={}".format(secondary_ip),
+             "index=2"])
     elif system == "Darwin":
         subprocess.run(["networksetup", "-setdnsservers", "Wi-Fi", primary_ip, secondary_ip])
     elif system == "Linux":
@@ -18,6 +25,10 @@ def set_dns(primary_ip, secondary_ip):
 def unset_dns():
     system = platform.system()
     if system == "Windows":
+        if not pyuac.isUserAdmin():
+            print("Re-launching as admin!")
+            pyuac.runAsAdmin()
+            return
         subprocess.run(["netsh", "interface", "ip", "set", "dns", "name=\"Wi-Fi\"", "dhcp"])
     elif system == "Darwin":
         subprocess.run(["networksetup", "-setdnsservers", "Wi-Fi", "Empty"])
@@ -29,14 +40,14 @@ def main():
     args = sys.argv
     if len(args) == 1:
         unset_dns()
-    elif args == 3:
+    elif len(args) == 3:
         primary_dns = args[1]
         secondary_dns = args[2]
         set_dns(primary_dns, secondary_dns)
     else:
-        print("Error: Not Enough Arguments")
+        print(f"Error: Not Enough Arguments ({len(args)})")
         print("Usage -> `python3 core.py` to unset dns")
-        print("Usage -> `python3 core.py <primary_dns> <secondary_dns>` to unset dns")
+        print("Usage -> `python3 core.py <primary_dns> <secondary_dns>` to set dns")
 
 
 if __name__ == "__main__":
